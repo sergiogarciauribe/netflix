22edb51982bb0afbe6be8aaa2a22b5c7572c23e8
sergiogarciauribe <sergio.341822090@ucaldas.edu.co>
2024-08-22 13:37:50 -0500

    ultimos cambios

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import viteLogo from "/vite.svg";
 import Button from "@mui/material/Button";
 import "./App.css";
 import { movies } from "./constant/movies";
-import { Container, Grid, Stack } from "@mui/material";
+import { Container, Grid, Stack, Typography } from "@mui/material";
 import Movie from "./components/movie";
 import { Margin } from "@mui/icons-material";
 import ResponsiveAppBar from "./components/navbar";
+import { series } from "./constant/series";
 function App() {
   const [count, setCount] = useState(0);
 
   const [ListMovies, setLisMovies] = useState(movies);
+  const [ListSeries, setLisSeries] = useState(series);
 
   return (
     <>
@@ -26,41 +28,91 @@ function App() {
           </video>
         </Container>
       </Stack>
-      <Stack
-        sx={{
-          overflowX: "auto",
-          flexDirection: "row",
-          paddingX: 3,
-          paddingY: 3,
-        }}
-      >
-        {ListMovies.map((movie) => (
-          <Movie
-            key={movie.id}
-            image={movie.image}
-            title={movie.title}
-            genero={movie.genero}
-          />
-        ))}
-      </Stack>
-      <Stack
-        sx={{
-          overflowX: "auto",
-          flexDirection: "row",
-          paddingX: 3,
-          paddingY: 3,
-          marginTop: 8,
-        }}
-      >
-        {ListMovies.map((movie) => (
-          <Movie
-            key={movie.id}
-            image={movie.image}
-            title={movie.title}
-            genero={movie.genero}
-          />
-        ))}
-      </Stack>
+      <Container maxWidth="lg" sx={{ justifyContent: "center" }}>
+        <Typography
+          gutterBottom
+          variant="subtitle2"
+          component="h2"
+          fontWeight="bold"
+          fontSize={"1.8rem"}
+          color={"#fff"}
+          marginBottom={0}
+          marginTop={4}
+        >
+          Películas
+        </Typography>
+        <Stack
+          sx={{
+            overflowX: "auto",
+            flexDirection: "row",
+            paddingY: 1,
+            marginBottom: 5,
+            "&::-webkit-scrollbar": {
+              height: "8px",
+            },
+            "&::-webkit-scrollbar-track": {
+              background: "#f1f1f1",
+            },
+            "&::-webkit-scrollbar-thumb": {
+              backgroundColor: "#c61515",
+              borderRadius: "0",
+            },
+            "&::-webkit-scrollbar-thumb:hover": {
+              backgroundColor: "#911111",
+            },
+          }}
+        >
+          {ListMovies.map((movie) => (
+            <Movie
+              key={movie.id}
+              image={movie.image}
+              title={movie.title}
+              genero={movie.genero}
+            />
+          ))}
+        </Stack>
+        <Typography
+          gutterBottom
+          variant="subtitle2"
+          component="h2"
+          fontWeight="bold"
+          fontSize={"1.8rem"}
+          color={"#fff"}
+          marginBottom={0}
+        >
+          Series
+        </Typography>
+        <Stack
+          sx={{
+            overflowX: "auto",
+            flexDirection: "row",
+            paddingY: 3,
+            marginBottom: 3,
+            "&::-webkit-scrollbar": {
+              height: "8px",
+            },
+            "&::-webkit-scrollbar-track": {
+              background: "#f1f1f1",
+            },
+            "&::-webkit-scrollbar-thumb": {
+              backgroundColor: "#c61515",
+              borderRadius: "0",
+            },
+            "&::-webkit-scrollbar-thumb:hover": {
+              backgroundColor: "#911111",
+            },
+          }}
+        >
+          {ListSeries.map((movie) => (
+            <Movie
+              key={movie.id}
+              image={movie.image}
+              title={movie.title}
+              genero={movie.genero}
+            />
+          ))}
+        </Stack>
+      </Container>
     </>
   );
 }
---
+++ src/constant/series.js
@@ -0,0 +1,38 @@
+export const series = [
+  {
+    id: 1,
+    image: "https://image.tmdb.org/t/p/w780/wR7f51NSYoIWJFnkdEG1pzIRI2Y.jpg",
+    title: "Bridgerton",
+    genero: "Drama",
+  },
+  {
+    id: 2,
+    image: "https://image.tmdb.org/t/p/w780/fAos5hPi7TB49KpuIAjvQNZkvwM.jpg",
+    title: "La Casa del Dragón",
+    genero: "Accion y Aventura",
+  },
+  {
+    id: 3,
+    image: "https://image.tmdb.org/t/p/w780/vKJVL5EY5lAdN2TtyqQvsvAwWFH.jpg",
+    title: "Taylor Swift vs Scooter",
+    genero: "Crimen",
+  },
+  {
+    id: 4,
+    image: "https://image.tmdb.org/t/p/w780/qXJnvVgdV95kau1Z6OKf1jq8oqa.jpg",
+    title: "Orphan Black: Echoes",
+    genero: "Drama",
+  },
+  {
+    id: 5,
+    image: "https://image.tmdb.org/t/p/w780/cBPmGCrIjVpqMxs8p6U4AkGtGyr.jpg",
+    title: "Ni una más",
+    genero: "Drama",
+  },
+  {
+    id: 6,
+    image: "https://image.tmdb.org/t/p/w780/53aonG0QS3ynbYuuwhPtyoOwTDD.jpg",
+    title: "Loki",
+    genero: "Fantasía",
+  },
+];
