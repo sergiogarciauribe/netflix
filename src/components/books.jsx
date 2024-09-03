8a5cb9a5d759f6ee84fbf31f4e00409231b356a4
sergiogarciauribe <sergio.341822090@ucaldas.edu.co>
2024-09-03 07:00:02 -0500

    colores en textfield

--- src/components/books.jsx
+++ src/components/books.jsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import {
   Avatar,
+  colors,
+  Container,
   Grid,
   IconButton,
   List,
@@ -8,10 +10,11 @@ import {
   ListItemAvatar,
   ListItemText,
   styled,
+  TextField,
   Typography,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
-import MenuBookIcon from '@mui/icons-material/MenuBook';
+import MenuBookIcon from "@mui/icons-material/MenuBook";
 
 const Demo = styled("div")(({ theme }) => ({
   backgroundColor: "#222",
@@ -20,57 +23,88 @@ const Demo = styled("div")(({ theme }) => ({
 const books = [
   {
     id: 1,
-    title: 'Harry Potter',
-    autorBook: 'J.K RowLing',
+    title: "Harry Potter",
+    autorBook: "J.K RowLing",
   },
   {
     id: 2,
-    title: 'The Lord of the Rings',
-    autorBook: 'J.R.R Tokien',
+    title: "The Lord of the Rings",
+    autorBook: "J.R.R Tokien",
   },
   {
     id: 3,
-    title: 'El señor de los anillos',
-    autorBook: 'J.R.R Tokien',
+    title: "El señor de los anillos",
+    autorBook: "J.R.R Tokien",
   },
-
-]
-
+];
 
 const Books = () => {
   const [ListBooks, setBooks] = React.useState(books);
 
   return (
     <>
-      <Grid item xs={12} md={6}>
-        <Typography color={"#fff"} sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-          Avatar with text and icon
-        </Typography>
-        <Demo>
-          <List>
-            {ListBooks.map((book) => (
-            <ListItem key={book.id}  sx={{color:"#fff"}}
-              secondaryAction={
-                <IconButton edge="end" aria-label="delete">
-                  <DeleteIcon sx={{color:"#fff" }}  />
-                </IconButton>
-              }
+      <Container maxWidth="xl">
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <Typography
+              color={"#fff"}
+              sx={{ mt: 4, mb: 2 }}
+              variant="h6"
+              component="div"
             >
-              <ListItemAvatar>
-                <Avatar>
-                  <MenuBookIcon/>
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText 
-                primary={book.title}
-                secondary={book.autorBook}
-                secondaryTypographyProps={{ color: '#ff2' }}
-              />
-            </ListItem>
-           ))}  
-          </List>
-        </Demo>
-      </Grid>
+              Mi libreria 📚📚
+            </Typography>
+            <Demo>
+              <List>
+                {ListBooks.map((book) => (
+                  <ListItem
+                    key={book.id}
+                    sx={{ color: "#fff" }}
+                    secondaryAction={
+                      <IconButton edge="end" aria-label="delete">
+                        <DeleteIcon sx={{ color: "#fff" }} />
+                      </IconButton>
+                    }
+                  >
+                    <ListItemAvatar>
+                      <Avatar>
+                        <MenuBookIcon sx={{ color: "#222" }} />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary={book.title}
+                      secondary={book.autorBook}
+                      secondaryTypographyProps={{ color: "#ff2" }}
+                    />
+                  </ListItem>
+                ))}
+              </List>
+            </Demo>
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <Typography
+              sx={{ mt: 4, mb: 2 }}
+              color={"#fff"}
+              variant="h6"
+              component={"div"}
+            >
+              Formulario
+            </Typography>
+            <TextField
+              label="Titulo"
+              color="warning"
+              focused
+              id="outlined-size-small"
+              size="small"
+              placeholder="Titulo"
+              InputProps={{
+                style: { color: "#FFF" },
+              }}
+            />
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 };
